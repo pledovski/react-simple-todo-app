3f03bd2424f0bb50e10e9e718be0cc4c8efc47a5
pledovski <human.pled@gmail.com>
2019-05-24 18:36:00 +0300

    TodoItems added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Dinner with Nastya',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'Wash dishes',
+        completed: true
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello!</h1>
+        <Todos todos={this.state.todos} />
       </div>
-    )
+    );
   }
 }
 
